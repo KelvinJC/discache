f0a6b28fac8b015368a2378310ac31c98cca19ac
kaizen <ojkelvinchu@gmail.com>
2026-02-24 19:10:02 +0100

    Test put/2, get/1 and has_key?/1

--- test/discache_test.exs
+++ test/discache_test.exs
@@ -2,7 +2,21 @@ defmodule DiscacheTest do
   use ExUnit.Case
   doctest Discache
 
-  test "greets the world" do
-    assert Discache.hello() == :world
+  test "has_key? returns true or false" do
+    value = Discache.has_key?(:user_cache)
+    assert is_boolean(value)
+  end
+
+  test "has_key? returns false for uncached keys" do
+    assert Discache.has_key?(:not_yet) == false
+  end
+
+  test "put/2 successful" do
+    assert Discache.put(:user_cache, "user:123") == :ok
+  end
+
+  test "get/1 successful" do
+    assert Discache.put(:get_cache, "user:123") == :ok
+    assert Discache.get(:get_cache) == {:ok, "user:123"}
   end
 end
